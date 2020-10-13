01762aef5d829b7dad94d963afcfd53501f4338b
Maxime Sagnard <maxime.sagnard@outlook.fr>
2020-10-13 08:38:25 +0200

    V1.1

--- PNRSorter/MVVM/ViewModel.cs
+++ PNRSorter/MVVM/ViewModel.cs
@@ -3,6 +3,7 @@ using PNRSorter.Utility;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
@@ -15,7 +16,7 @@ namespace PNRSorter.MVVM
 {
     public class ViewModel : VMBase
     {
-        
+
         #region Fields
         private ObservableCollection<MyPNR> _myPNR;
         private List<MyPNR> _PMS;
@@ -23,6 +24,15 @@ namespace PNRSorter.MVVM
         private List<MyPNR> _KE24;
         private List<string> _PNRPMS;
         private List<string> _PNRSNC;
+        private List<string> _uniquePNR;
+        private List<string> _toBeSorted;
+        private string _PNRSearch;
+        private ObservableCollection<string> _PNRList;
+        private int _salesCol;
+        private int _productCol;
+        private int _unitSoldCol;
+        private int _colNb;
+        private Dictionary<string, int> _colInfo = new Dictionary<string, int>();
         #endregion
 
         #region Properties
@@ -56,6 +66,31 @@ namespace PNRSorter.MVVM
             get => _PNRSNC;
             set { _PNRSNC = value; OnPropertyChanged("PNRSNC"); }
         }
+        public string PNRSearch
+        {
+            get => _PNRSearch;
+            set { _PNRSearch = value; OnPropertyChanged("PNRSearch"); }
+        }
+        public List<string> UniquePNR
+        {
+            get => _uniquePNR;
+            set { _uniquePNR = value; OnPropertyChanged("UniquePNR"); }
+        }
+        public List<string> ToBeSorted
+        {
+            get => _toBeSorted;
+            set { _toBeSorted = value; OnPropertyChanged("ToBeSorted"); }
+        }
+        public ObservableCollection<string> PNRList
+        {
+            get => _PNRList;
+            set { _PNRList = value; OnPropertyChanged("PNRList"); }
+        }
+        public Dictionary<string, int> colInfo
+        {
+            get => _colInfo;
+            set { _colInfo = value; OnPropertyChanged("colInfo"); }
+        }
         #endregion
 
         #region Commands
@@ -67,19 +102,32 @@ namespace PNRSorter.MVVM
         {
             //Required for EEPLUS to be used
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
             //Commands
             TestCommand = new RelayCommand(o => test(), o => true);
-
             //Extracting data
             FileInfo SNCFile = new FileInfo(@"C:\Users\msag\Desktop\PNRSorter\Families_S_C_v3.xlsx");
             FileInfo PMSFile = new FileInfo(@"C:\Users\msag\Desktop\PNRSorter\Families_PMS_v3.xlsx");
             FileInfo KE24File = new FileInfo(@"C:\Users\msag\Desktop\PNRSorter\KE24_2020.xlsx");
+            //List for the autocompletion
+            PNRList = new ObservableCollection<string>();
+            //List of already classified PNRs
             PNRPMS = new List<string>();
             PNRSNC = new List<string>();
             SNC = ExtractedPNR(SNCFile, "S&C");
             PMS = ExtractedPNR(PMSFile, "PMS");
-            KE24 = ExtractedPNR(KE24File, "/", "Product");
+            //Selecting the desired values
+            colInfo = new Dictionary<string, int>();
+            colInfo.Add("Product", -1);
+            colInfo.Add("Sales", -1);
+            colInfo.Add("Billing Quantity", -1);
+            FindColumn(KE24File, colInfo);
+            foreach (var key in colInfo.Keys)
+                Console.WriteLine("key: " + key + ", colNb: " + colInfo[key].ToString());
+            KE24 = ExtractedPNR(KE24File, "/", selectedColumns);
+            //Isolating PNRs
+            UniquePNR = FindUnique(KE24);
+            ToBeSorted = FilterExistingPNR(UniquePNR);
+            //Verification procedures
         }
         #endregion
 
@@ -93,63 +141,53 @@ namespace PNRSorter.MVVM
         public void test()
         {
             //var file = new FileInfo(@"L:\Engineering_Energy\Monthly_ProductLine_Reviews\_Dashboard\Families_PMS_v2.xlsx");
-            List<string> uniquePNR = FindUnique(KE24);
-            List<string> toBeSorted = FilterExistingPNR(uniquePNR);
-            Console.WriteLine("total unique: " + uniquePNR.Count());
-            Console.WriteLine("total to be sorted: " + toBeSorted.Count());
+            Console.WriteLine("total unique: " + UniquePNR.Count());
+            Console.WriteLine("total to be sorted: " + ToBeSorted.Count());
         }
 
         #region Private Methods
-        private List<MyPNR> ExtractedPNR(FileInfo file, string group, string columnName = "")
+        private List<MyPNR> ExtractedPNR(FileInfo file, string group, Dictionary<string,int> colInfo = null)
         {
             List<MyPNR> extractedPNR = new List<MyPNR>();
             using (var package = new ExcelPackage(file))
             {
                 ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
                 int rows = worksheet.Dimension.Rows;
-                int columns = worksheet.Dimension.Columns;
-                for (int i = 1; i < columns + 1; i++)
+                _colNb = worksheet.Dimension.Columns;
+                for (int i = 1; i < _colNb + 1; i++)
                 {
-                    if(columnName == "")
+                    if (colInfo == null)
                     {
-                        for (int j = 1; i < rows + 1; j++)
+                        for (int j = 1; j < rows + 1; j++)
                         {
-                            try
+                            MyPNR curPNR = new MyPNR();
+                            if (worksheet.Cells[j, i].Value != null)
                             {
-                                MyPNR curPNR = new MyPNR();
+                                curPNR.PNR = worksheet.Cells[j, i].Value.ToString();
                                 curPNR.Group = group;
                                 curPNR.Family = worksheet.Cells[1, i].Value.ToString();
-                                curPNR.PNR = worksheet.Cells[j, i].Value.ToString();
                                 extractedPNR.Add(curPNR);
                                 if (group == "S&C")
                                     PNRSNC.Add(curPNR.PNR);
                                 else
                                     PNRPMS.Add(curPNR.PNR);
                             }
-                            catch
-                            {
-                                break;
-                            }
                         }
                     }
-
+                    //if in this loop, we are going inside of the main file -- pbly KE24
                     else
                     {
-                        if(worksheet.Cells[1,i].Value.ToString() == columnName)
+                        if (colInfo.Keys.Contains(worksheet.Cells[1, i].Value.ToString()))
                         {
-                            for (int j = 1; i < rows +1; j++)
+                            for (int j = 1; j < rows + 1; j++)
                             {
-                                try
+                                MyPNR curPNR = new MyPNR();
+                                if (worksheet.Cells[j, i].Value != null)
                                 {
-                                    MyPNR curPNR = new MyPNR();
                                     curPNR.Group = "unknown";
                                     curPNR.Family = "unknown";
                                     curPNR.PNR = worksheet.Cells[j, i].Value.ToString();
                                     extractedPNR.Add(curPNR);
-                                }
-                                catch
-                                {
-                                    break;
                                 }
                             }
                         }
@@ -162,11 +200,12 @@ namespace PNRSorter.MVVM
         private List<string> FindUnique(List<MyPNR> pnrList)
         {
             List<string> uniquePNR = new List<string>();
-            foreach(var pnr in pnrList)
+            foreach (var pnr in pnrList)
             {
                 if (!uniquePNR.Contains(pnr.PNR))
                 {
                     uniquePNR.Add(pnr.PNR);
+                    PNRList.Add(pnr.PNR);
                 }
             }
 
@@ -176,7 +215,7 @@ namespace PNRSorter.MVVM
         private List<string> FilterExistingPNR(List<string> uniquePNR)
         {
             List<string> toBeSorted = new List<string>();
-            foreach(var pnr in uniquePNR)
+            foreach (var pnr in uniquePNR)
             {
                 if (!(PNRPMS.Contains(pnr) || PNRSNC.Contains(pnr)))
                     toBeSorted.Add(pnr);
@@ -185,24 +224,38 @@ namespace PNRSorter.MVVM
             return toBeSorted;
         }
 
-        private double FindColumn(FileInfo excel, string name)
+        private void FindColumn(FileInfo excel, Dictionary<string, int> name)
         {
-            double columnNb = 0;
             using (var package = new ExcelPackage(excel))
             {
                 ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
                 int columns = worksheet.Dimension.Columns;
                 for (int i = 1; i < columns + 1; i++)
                 {
-                    if (name == "")
+                    if (name.Keys.Contains(worksheet.Cells[1, i].Value.ToString()))
                     {
-                        columnNb = i;
-                        break;
+                        name[worksheet.Cells[1, i].Value.ToString()] = i;
                     }
                 }
             }
 
-            return columnNb;
+            return;
+        }
+        #endregion
+
+        #region Research
+        private MyPNR PNRFetcher(FileInfo excel, string pnrNb)
+        {
+            using (var package = new ExcelPackage(excel))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                for (int j = 1; j < (_colNb + 1); j++)
+                {
+                    //if(worksheet.Cells[j, _productCol].Value.ToString() == pnrNb)
+                }
+            }
+            MyPNR pnr = new MyPNR();
+            return pnr;
         }
         #endregion
     }
